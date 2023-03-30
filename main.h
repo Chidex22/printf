3d9a817ea5ea2afd247822b139397063df63f733
Chidex22 <chidex7119@gmail.com>
2023-03-30 11:58:56 -0700

    main.h

--- main.h
+++ main.h
@@ -72,11 +72,11 @@ int print_hexa(va_list types, char map_to[],
 
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* Funcion to print memory address */
 int print_pointer(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);
@@ -86,21 +86,21 @@ int get_size(const char *format, int *i);
 
 /*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
-/* width handler */
+/* width strong handler */
 int handle_write_char(char c, char buffer[],
-			int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
-			int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-			int length, char padd, char extra_c);
+		int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision
+		int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
-			int width, int flags, char padd, char extra_c, int padd_start);
+		int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
 		char buffer[],
